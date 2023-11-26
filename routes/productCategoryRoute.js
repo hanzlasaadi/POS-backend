0fda7befaa9acbd83a382d7bd88c859de248cf5a
Hanzla Saadi <hanzalasaadi@gmail.com>
2023-11-26 05:20:12 +0500

    auth for category & subcategory

--- routes/productCategoryRoute.js
+++ routes/productCategoryRoute.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const productCategoryController = require('../controllers/productCategoryController');
+const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-router
-  .route('/')
-  .get(productCategoryController.getAllProductCategory)
-  .post(productCategoryController.newProductCategory);
+router.route('/').get(productCategoryController.getAllProductCategory);
 
+router.route('/:id').get(productCategoryController.getOne);
+
+// verify all requests below and only admin can make these requests
+router.use(
+  authController.verify,
+  authController.checkAdmin('admin', 'developer'),
+);
+
+router.route('/').post(productCategoryController.newProductCategory);
 router
   .route('/:id')
-  .get(productCategoryController.getOne)
   .patch(productCategoryController.updateProductCategory)
   .delete(productCategoryController.deleteProductCategory);
 
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// unprotected routes
+
+router.route('/').get(productController.getAllProduct);
+
+router.route('/:id').get(productController.getOneProduct);
+
+// protected routes (admin and developer)
+router.use(
+  authController.verify,
+  authController.checkAdmin('worker', 'admin', 'developer'),
+);
+
 router.route('/stats').get(productController.statsProduct);
 
-router
-  .route('/')
-  .get(productController.getAllProduct)
-  .post(productController.newProduct);
+router.route('/').post(productController.newProduct);
 
 router
   .route('/:id')
-  .get(productController.getOneProduct)
   .patch(productController.updateProduct)
   .delete(productController.deleteProduct);
 
